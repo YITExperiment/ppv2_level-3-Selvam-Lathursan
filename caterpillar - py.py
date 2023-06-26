a1dd9686c6bfaea0d4e6e4efd6b277ef111a565b
mANIVATHANAN vAKEETHAN <135089116+Selvam-Lathursan@users.noreply.github.com>
2023-06-26 13:38:42 +0530

    Add files via upload

--- caterpillar - py.py
+++ caterpillar - py.py
@@ -6,7 +6,7 @@ t.bgcolor('yellow')
 caterpillar=t.Turtle()
 caterpillar.shape('square')
 caterpillar.color('red')
-caterpillar.speed(0)
+caterpillar.speed(5)
 caterpillar.penup()
 caterpillar.hideturtle()
 
@@ -17,10 +17,10 @@ leaf.shape('leaf')
 leaf.color('green')
 leaf.penup()
 leaf.hideturtle()
-leaf.speed()
+leaf.speed(3)
 
 game_started=False
-text_turtle = t.Turtle()
+text_turtle=t.Turtle()
 text_turtle.write('Press SPACE to start',align='center',font=('Arial',16,'bold'))
 text_turtle.hideturtle()
 
@@ -34,15 +34,10 @@ def outside_window():
    top_wall=t.window_height()/2
    bottom_wall=t.window_height()/2
    (x,y)= caterpillar.pos()
-   outside=x<left_wall or x>right_wall or y<bottom_wall or y>top_wall
+   outside=x < left_wall or x > right_wall or y < bottom_wall or y >top_wall
    return outside
 
-def game_over():
-    caterpillar.color('yellow')
-    leaf.color('yellow')
-    t.penup()
-    t.hideturtle()
-    t.write('GAME OVER!',align='center',font=('Aerial',30,'normal'))
+
 
 def display_score(current_score):
    score_turtle.clear()
@@ -64,7 +59,7 @@ def start_game():
      return
    game_started=True
 
-   score=0
+   score=10
    text_turtle.clear()
 
    caterpillar_speed=2
@@ -75,18 +70,19 @@ def start_game():
    place_leaf()
 
    while True:
-      caterpillar.forward(caterpillar_speed)
-      if caterpillar.distance(leaf)<20:
+     caterpillar.forward(caterpillar_speed)
+     if caterpillar.distance(leaf)<10:
+
 
        place_leaf()
        caterpillar_length= caterpillar_length+1
        caterpillar.shapesize(1,caterpillar_length,1)
-       caterpillar_speed=caterpillar_speed+1
+       caterpillar_speed=caterpillar_speed+9
        score=score+10
        display_score(score)
-      if outside_window():
-        game_over()
-        break
+     if outside_window():
+       game_over()
+       break
 
 def move_up():
    if caterpillar.heading()==0 or caterpillar.heading()==180:caterpillar.setheading(90)
